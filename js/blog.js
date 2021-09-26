2e9e4c761faf77b362cee1dc5f18a748584b6a53
Soph H.P <smkendall90@gmail.com>
2021-09-26 17:01:55 +0200

    added border to selected catagiry filters

--- js/blog.js
+++ js/blog.js
@@ -8,10 +8,20 @@ renderBlogPosts(fetchPosts(postsUrl + order));
 //Filter categories buttons
 
 const allCategoriesButton = document.querySelector("#all-categories");
+allCategoriesButton.style.border = "solid 4px var(--color-1)";
+const bugsWithBugsButton = document.querySelector("#bugs-with-bugs");
+const bugsAreCodeButton = document.querySelector("#bugs-that-are-code");
+const typesOfBugButton = document.querySelector("#types-of-bug");
+const bugHistoryButton = document.querySelector("#bug-history");
 let currentCategory = null;
 let categoryUrl;
 
 allCategoriesButton.addEventListener("click", () => {
+  bugsWithBugsButton.style.border = "none";
+  bugsAreCodeButton.style.border = "none";
+  typesOfBugButton.style.border = "none";
+  bugHistoryButton.style.border = "none";
+  allCategoriesButton.style.border = "solid 4px var(--color-1)";
   renderBlogPosts(fetchPosts(postsUrl + order));
   currentCategory = null;
   offsetPosts = 0;
@@ -25,12 +35,18 @@ let viewMoreButtonUrl;
 const categoryFilterButtons = document.querySelectorAll(".category_filter");
 categoryFilterButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
+    allCategoriesButton.style.border = "none";
+    bugsWithBugsButton.style.border = "none";
+    bugsAreCodeButton.style.border = "none";
+    typesOfBugButton.style.border = "none";
+    bugHistoryButton.style.border = "none";
     postsToView = 6;
     offsetPosts = 0;
     const categoryId = event.target.dataset.category;
     currentCategory = categoryId;
     categoryUrl = `https://soph-web-dev.eu/bug-blog/wp-json/wp/v2/posts?_embed&categories=${categoryId}&per_page=6`;
     renderBlogPosts(fetchPosts(categoryUrl + order));
+    button.style.border = "solid 5px var(--color-5)";
   });
 });
 
